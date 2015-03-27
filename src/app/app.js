636de420e02c90c6ae525d303b3b94c2e44ea549
adeler <ire.alvarado@gmail.com>
2015-03-27 15:54:46 -0400

    Added functionality to make API call to Google Places Autocomplete service

--- src/app/app.js
+++ src/app/app.js
@@ -1,27 +1,118 @@
+(function() {
 
-$(document).ready(function(){/* google maps -----------------------------------------------------*/
-google.maps.event.addDomListener(window, 'load', initialize);
+    /**
+     * ViewModel associated to the YouTube player section
+     */
+    var ResultsViewModel = function() {
+        var self = this;
+        this.map;
+        this.infowindow ;
+        this.service;
 
-function initialize() {
+        this.userSearch = ko.observable(); // Input by user in the search bar
+        this.placesResultsList = ko.observableArray([]); // {marker,html}
+        var markers = [];
 
-  /* position Amsterdam */
-  var latlng = new google.maps.LatLng(52.3731, 4.8922);
+        //Initial settings for example
+        this.initialRequest = "Coffee near San Francisco" ;
+        this.initialLat = 37.7577 ;
+        this.initialLng = -122.4376 ;
 
-  var mapOptions = {
-    center: latlng,
-    scrollWheel: false,
-    zoom: 13
-  };
-  
-  var marker = new google.maps.Marker({
-    position: latlng,
-    url: '/',
-    animation: google.maps.Animation.DROP
-  });
-  
-  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-  marker.setMap(map);
+        /**
+         * Timeout function in case Google Maps doesn't load
+         */
+        this.mapTimeout = setTimeout(function() {
+            $('#map-canvas').html('Problem loading Google Maps. Please refresh your browser and try again.');
+        }, 6000);
 
-};
-/* end google maps -----------------------------------------------------*/
-});
+        this.initializeMap = function() {
+            var mapOptions = {
+                center: new google.maps.LatLng(self.initialLat, self.initialLng),
+                zoom: 10
+            };
+            self.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+            clearTimeout(self.mapTimeout);
+
+            self.infowindow = new google.maps.InfoWindow();
+
+            //Initial example request
+            self.userSearch(self.initialRequest) ;
+            self.searchLocation() ;
+            //
+
+        }.bind(this);
+
+        // https://developers.google.com/maps/documentation/javascript/places-autocomplete
+        this.searchLocation = function() {
+            self.service = new google.maps.places.AutocompleteService();
+            console.log("calling the service");
+            self.service.getPlacePredictions({
+                input: self.userSearch()
+            }, self.callback);
+        }.bind(this);
+
+        this.removeMarkers = function() {
+            for (var i = 0; i < markers.length; i++) {
+                markers[i].setMap(null);
+            }
+
+            markers = [] ;
+
+        }.bind(this);
+
+        this.callback = function(results, status) {
+            //console.log(status) ;
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                console.log(results);
+                self.removeMarkers() // Removes the markers from the map,
+                var bounds = new google.maps.LatLngBounds();
+
+                var service2 = new google.maps.places.PlacesService(self.map);
+
+                for (var i = 0; i < results.length; i++) {
+
+                    var request = {
+                        placeId: results[i].place_id
+                    }
+
+                    service2.getDetails(request, function(place, status) {
+                        if (status == google.maps.places.PlacesServiceStatus.OK) {
+                            var marker = new google.maps.Marker({
+                                map: self.map,
+                                position: place.geometry.location,
+                                animation: google.maps.Animation.DROP
+                            });
+
+                            markers.push(marker);
+
+                            bounds.extend(marker.position);
+
+                            if(markers.length == results.length) {
+                                console.log("complete") ;
+                                self.map.fitBounds(bounds);
+                            }
+
+                            google.maps.event.addListener(marker, 'click', function() {
+                                self.infowindow.setContent(place.name);
+                                self.infowindow.open(self.map, this);
+                            });
+                        }
+                    });
+
+                    console.log(results[i].place_id);
+                }
+            }
+        }.bind(this);
+
+        google.maps.event.addDomListener(window, 'load', this.initializeMap);
+    };
+
+
+    ko.applyBindings(new ResultsViewModel());
+
+    //var resultsViewModel = new ResultsViewModel();
+    //var mapViewModel = new MapViewModel();
+    //ko.applyBindings(resultsViewModel, $('#playerContainer')[0]); //Apply our bindings to the player container
+    //ko.applyBindings(mapViewModel, $('#topVideos')[0]); //Apply our bindings to the top ten video section
+
+}());
