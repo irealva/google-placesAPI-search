b88689d725e0ea50b940891bda3e6707f05f3243
adeler <ire.alvarado@gmail.com>
2015-03-29 19:55:49 -0400

    Fixing README with live link

--- README.md
+++ README.md
@@ -1,18 +1,17 @@
 ## Google Places API Search
 
-This single-page application shows a list of all the public restrooms in NYC parks and their location on a Google Map. Users can filter by borough or search through the list. Clicking on a map marker reveals information about the restroom, as well as local venues and pictures of the area.
+This single-page application lets you query the Google Places API and renders a list and google map with the search results. I mainly used Knockout, Bootstrap, the Google Maps API, and the Google Places API. The app is also responsive, try it on different devices.
 
-Link to live page: [http://irealva.github.io/nyc-map-project/dist/](http://irealva.github.io/nyc-map-project/dist/)
+Link to live page: [http://irealva.github.io/google-placesAPI-search/dist/](http://irealva.github.io/google-placesAPI-search/dist/)
 
-
-###### What you need:
+#### What you need:
 npm
 
 Bower
 
 Gulp
 
-######To create the development version, from the root folder:
+#### To create the development version, from the root folder:
 
 1) Install the dependencies into the local node_modules folder, based on the package.json file
 
@@ -20,22 +19,10 @@ Gulp
 
 2) Install packages to their newest versions according to bower.json
 
-`bower update`
+`bower install`
 
 3) Use gulp to create the /dist folder
 
 `gulp`
 
-4) Open /dist/index.html to see the map.
-
-####References used
-
-Udacity website
-
-KnockoutJS documentation
-
-Foursquare API documentation
-
-Google Maps APi documentation
-
-Yeoman generator for KnockoutJS
+4) Open /dist/index.html
--- src/app/app.js
+++ src/app/app.js
@@ -1,19 +1,24 @@
+/** 
+ * app.js
+ * Single page app to display the results of a query to the Google Places API
+ * @author Irene Alvarado
+ */
+
 (function() {
 
     /**
-     * ViewModel associated to the YouTube player section
+     * ViewModel the application
      */
     var ResultsViewModel = function() {
         var self = this;
         this.map;
         this.infowindow;
-        this.service;
 
         this.userSearch = ko.observable(); // Input by user in the search bar
-        this.placesResultsList = ko.observableArray([]); // {marker, header, description}
-        var markers = [];
+        this.placeholder = ko.observable(); //Placeholder text in the search bar
+        this.placesResultsList = ko.observableArray([]); // will contain search result objects of type { marker, header, description}
 
-        //Initial settings for example
+        //Initial settings to show as an example when page renders for the first time
         this.initialRequest = "Coffee near San Francisco";
         this.initialLat = 37.7577;
         this.initialLng = -122.4376;
@@ -25,91 +30,116 @@
             $('#map-canvas').html('Problem loading Google Maps. Please refresh your browser and try again.');
         }, 6000);
 
+
+        /**
+         * Initialize google maps and perform an example search request
+         */
         this.initializeMap = function() {
             var mapOptions = {
                 center: new google.maps.LatLng(self.initialLat, self.initialLng),
                 zoom: 10
             };
             self.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-            clearTimeout(self.mapTimeout);
+            clearTimeout(self.mapTimeout); // clear the timeout if the map loads
 
             self.infowindow = new google.maps.InfoWindow();
+
+            // Placeholder text in search bar
+            self.placeholder("Search for a place") ; 
 
             //Initial example request
             self.userSearch(self.initialRequest);
             self.searchLocation();
-            //
-
         }.bind(this);
 
-        // https://developers.google.com/maps/documentation/javascript/places-autocomplete
+        /**
+         * Perform a search request to the Places API using Google's Autocomplete service
+         * https://developers.google.com/maps/documentation/javascript/places-autocomplete
+         */
         this.searchLocation = function() {
-            self.service = new google.maps.places.AutocompleteService();
-            console.log("calling the service");
-            self.service.getPlacePredictions({
+            var service = new google.maps.places.AutocompleteService();
+
+            service.getPlacePredictions({
                 input: self.userSearch()
             }, self.callback);
         }.bind(this);
 
+        /**
+         * Remove Google Map markers when a user performs a new search request
+         */
         this.removeMarkers = function() {
-            for (var i = 0; i < markers.length; i++) {
-                markers[i].setMap(null);
-            }
+            ko.utils.arrayForEach(self.placesResultsList(), function(item) { // Search through the array containing our search results
+                item.marker.setMap(null);
+            });
 
-            markers = [];
             self.placesResultsList.removeAll();
         }.bind(this);
 
+        /**
+         * Callback function for getPlacePredictions() Google Places API call. 
+         * @param {object} results JSON object containing the results from the API call
+         * @param {string} status Status message for the API call
+         */
         this.callback = function(results, status) {
-            //console.log(status) ;
             if (status == google.maps.places.PlacesServiceStatus.OK) {
-                console.log(results);
+
                 self.removeMarkers() // Removes the markers from the map,
-                var bounds = new google.maps.LatLngBounds();
 
-                var service2 = new google.maps.places.PlacesService(self.map);
+                var bounds = new google.maps.LatLngBounds(); // Sets the map bounds
+                var service = new google.maps.places.PlacesService(self.map);
 
+                // Iterate through each location in the 'results' object and call the PlaceService() function to get 
+                // detailed information about a location
                 for (var i = 0; i < results.length; i++) {
-
                     var request = {
                         placeId: results[i].place_id
                     }
 
-                    service2.getDetails(request, function(place, status) {
+                    //Get detailed information about a location by making another API call to the Google Places API
+                    service.getDetails(request, function(place, status) {
                         if (status == google.maps.places.PlacesServiceStatus.OK) {
                             var marker = new google.maps.Marker({
                                 map: self.map,
                                 position: place.geometry.location,
                                 animation: google.maps.Animation.DROP
-                            });
+                            }); //Create a marker for the new location
 
                             self.placesResultsList.push({
                                 marker: marker,
                                 html: self.getDescriptionString(place)
-                            });
+                            }); //Add the marker and description of the location to our results list
 
-                            markers.push(marker);
+                            bounds.extend(marker.position); // Extend the map bounds to include the new location
 
-                            bounds.extend(marker.position);
-
-                            if (markers.length == results.length) {
-                                console.log("complete");
+                            if (self.placesResultsList().length == results.length) { // Fit the map to the new bounds
                                 self.map.fitBounds(bounds);
                             }
 
                             google.maps.event.addListener(marker, 'click', function() {
                                 self.infowindow.setContent(place.name);
                                 self.infowindow.open(self.map, this);
-                            });
+                            }); // Add an event listener in case marker is clicked
+                        }
+                        else {
+                            alert("Error obtaining some location details: " + status) ;
                         }
                     });
-
-                    console.log(results[i].place_id);
                 }
+            }
+            else if (status == google.maps.places.PlacesServiceStatus.ZERO_RESULTS) { //If no results back from the Google Places request
+                self.placeholder("No results for that search. Try again.") ; 
+                self.userSearch("") ;
+            }
+            else {
+                alert("Error loading results from the Google Places Library: " + status) ;
             }
         }.bind(this);
 
-
+        /**
+         * Creates a string with the description of a location
+         * @param {object} place JSON object representing a location
+         * @return {string} string to display as HTML
+         */
         this.getDescriptionString = function(place) {
             var name = place.name;
             var address = place.formatted_address;
@@ -119,7 +149,7 @@
 
             var description = "<p>" + address + "<br>";
 
-            if (typeof phone_number !== 'undefined') {
+            if (typeof phone_number !== 'undefined') { // Check if the location contains phone or permanently closed information
                 description += phone_number + "<br>";
 
                 if (typeof closed !== 'undefined') {
@@ -129,7 +159,7 @@
 
             description += "</p>";
 
-            if (typeof website !== 'undefined') {
+            if (typeof website !== 'undefined') { // Link to a location website
                 description += "<a href='" + website + "'>" + website + "</a>";
             }
 
@@ -142,6 +172,10 @@
 
         }.bind(this);
 
+        /**
+         * Simulate a marker click if a user clicks on a location header from the list of search results
+         * @param {object} clickedItem the location that a user clicked
+         */
         this.clickedHeader = function(clickedItem) {
             google.maps.event.trigger(clickedItem.marker, 'click');
         }.bind(this);
@@ -149,6 +183,5 @@
         google.maps.event.addDomListener(window, 'load', this.initializeMap);
     };
 
-
-    ko.applyBindings(new ResultsViewModel());
+    ko.applyBindings(new ResultsViewModel()); //Apply knockout bindings
 }());
